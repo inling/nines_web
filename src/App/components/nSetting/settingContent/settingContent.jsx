b4867be04c020bf6b4fcdfa45a4207d3867ceed7
inling <giraffe.aup@gmail.com>
2020-04-09 20:15:30 +0800

    个人信息

--- src/App/components/nSetting/settingContent/settingContent.jsx
+++ src/App/components/nSetting/settingContent/settingContent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Row, Col, Avatar, Divider, Button } from 'antd';
-import { MobileOutlined, LockOutlined,EditOutlined } from '@ant-design/icons';
+import { MobileOutlined, LockOutlined, EditOutlined, ReadOutlined, UserSwitchOutlined } from '@ant-design/icons';
 import './settingContent.less';
 const publc_url = process.env.PUBLIC_URL;
 class settingContent extends React.Component {
@@ -12,10 +13,37 @@ class settingContent extends React.Component {
                         <Avatar src={publc_url + '/20170729141852_HFzVE.jpeg'} size={106} alt="头像"></Avatar>
                     </Col>
                     <Col span={20}>
-                        <h1>Nines</h1>
-                        <h4 style={{ color: '#777777' }}>
-                            博客号：154523
-                        </h4>
+                        <Row>
+                            <Col span={24} className="user_info_p">
+                                <span className="uesr_n">Nines</span>
+                                <span className="c-lv">LV2</span>
+                                <Link to="/">
+                                    <EditOutlined className="user_edit" />
+                                </Link>
+                            </Col>
+                            <Col span={24} className="user_info_p">
+                                <div className="user_msg">
+                                    博客号：
+                                    <span className="blog_num">154523</span>
+                                    <Link to="/">
+                                        <EditOutlined className="user_edit" />
+                                    </Link>
+                                </div>
+                            </Col>
+                            <Col span={24} className="user_art">
+                                <Row>
+                                    <Col span={4}>
+                                        <ReadOutlined  className="user_art_icon"/>
+                                        <span className="user_art_icon_msg">12</span>
+                                    </Col>
+                                    <Col span={4}>
+                                        <Divider type="vertical" />
+                                        <UserSwitchOutlined  className="user_art_icon" />
+                                        <span className="user_art_icon_msg">12</span>
+                                    </Col>
+                                </Row> 
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
                 <Row className="userInfoList">
@@ -24,12 +52,14 @@ class settingContent extends React.Component {
                             <Col span={2}>
                                 基础信息
                             </Col>
-                            <Col span={18}  style={{ padding:'0 12px'}}>
+                            <Col span={18} style={{ padding: '0 12px' }}>
                                 <Divider orientation="left"></Divider>
                             </Col>
-                            <Col span={4} style={{ fontSize: '12px',padding:'0 12px',color:'#999'}}>
-                                <EditOutlined style={{color:'#1890ff',fontSize:'14px'}}/>
-                                编辑资料
+                            <Col span={4} style={{ fontSize: '12px', padding: '0 12px', color: '#999' }}>
+                                <Link to="/">
+                                    <EditOutlined style={{ color: '#1890ff', fontSize: '16px' }} />
+                                    <span style={{ marginLeft: '5px' }}>编辑资料</span>
+                                </Link>
                             </Col>
                         </Row>
                     </Col>
@@ -60,45 +90,43 @@ class settingContent extends React.Component {
                             <Col span={2}>
                                 账号设置
                             </Col>
-                            <Col span={22}   style={{ padding:'0 12px'}}>
+                            <Col span={22} style={{ padding: '0 12px' }}>
                                 <Divider orientation="left"></Divider>
                             </Col>
                         </Row>
                     </Col>
                     <Col span={12}>
-                        <p>
-                            <Row align="middle">
-                                <Col span={3}>
-                                    <MobileOutlined style={{ fontSize: '32px', color: '#999' }} />
-                                </Col>
-                                <Col span={13}>
-                                    <p className="p1">修改绑定手机</p>
-                                    <p className="p2">已绑定：
+
+                        <Row align="middle">
+                            <Col span={3}>
+                                <MobileOutlined style={{ fontSize: '32px', color: '#999' }} />
+                            </Col>
+                            <Col span={12} className="msg">
+                                <p className="msg_1">修改绑定手机</p>
+                                <p className="msg_2">已绑定：
                                         <span>176****5739</span>
-                                    </p>
-                                </Col>
-                                <Col span={8}  style={{ textAlign: 'center'}}>
-                                    <Button size="small"  style={{ fontSize: '12px' }}>修改手机</Button>
-                                </Col>
-                            </Row>
-                        </p>
+                                </p>
+                            </Col>
+                            <Col span={9} style={{ textAlign: 'center' }}>
+                                <Button size="small" style={{ fontSize: '12px' }}>修改手机</Button>
+                            </Col>
+                        </Row>
+
                     </Col>
                     <Col span={12}>
-                        <p>
-                            <Row align="middle">
-                                <Col span={3}>
-                                    <LockOutlined style={{ fontSize: '32px', color: '#999' }} />
-                                </Col>
-                                <Col span={13}>
-                                    <p className="p1">修改密码</p>
-                                    <p className="p2">建议定期修改密码，保证账号安全</p>
-                                </Col>
-                                <Col span={8} style={{ textAlign: 'center'}}>
-                                    <Button  size="small" style={{ fontSize: '12px' }}>修改密码</Button>
-                                </Col>
-                            </Row>
 
-                        </p>
+                        <Row align="middle">
+                            <Col span={3}>
+                                <LockOutlined style={{ fontSize: '32px', color: '#999' }} />
+                            </Col>
+                            <Col span={12} className="msg">
+                                <p className="msg_1">修改密码</p>
+                                <p className="msg_2">建议定期修改密码，保证账号安全</p>
+                            </Col>
+                            <Col span={9} style={{ textAlign: 'center' }}>
+                                <Button size="small" style={{ fontSize: '12px' }}>修改密码</Button>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </div>
